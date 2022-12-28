3fd741ef8e58d03c6ed5b09a6810722c27685b2d
Michał Wodniczak <michal.wodniczak@o2.pl>
2022-12-28 17:42:09 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let button = document.querySelector(".js-buttonNewBackground");
+let header = document.querySelector(".js-mainHeader");
+let photoButton = document.querySelector(".js-buttonPhotoRemove");
+let photo = document.querySelector(".js-photo");
+let themeButtonElement = document.querySelector(".js-buttonChangeTheme");
+let themeTextElement = document.querySelector(".js-themeText");
+let body = document.querySelector(".body")
+
+button.addEventListener("click", () => {
+    header.classList.toggle("header__mainHeader--newColor");
+})
+
+photoButton.addEventListener("click", () => {
+    photo.remove();
+})
+
+themeButton.addEventListener("click", () => {
+    body.classList.toggle("body--newTheme");
+    body.classList.contains("body--newTheme") ? themeTextElement.innerText = ("Wyłącz") : themeTextElement.innerText = ("Włącz");
+})
