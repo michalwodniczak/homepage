6f8b71d74bc1aafe8c18e9a5efcabb2989361f31
Michał Wodniczak <michal.wodniczak@o2.pl>
2023-01-03 19:00:05 +0100

    added block

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
-let button = document.querySelector(".js-buttonNewBackground");
-let header = document.querySelector(".js-mainHeader");
-let photoButton = document.querySelector(".js-buttonPhotoRemove");
-let photo = document.querySelector(".js-photo");
-let themeButtonElement = document.querySelector(".js-buttonChangeTheme");
-let themeTextElement = document.querySelector(".js-themeText");
-let body = document.querySelector(".body")
+{
+    let button = document.querySelector(".js-buttonNewBackground");
+    let header = document.querySelector(".js-mainHeader");
+    let photoButton = document.querySelector(".js-buttonPhotoRemove");
+    let photo = document.querySelector(".js-photo");
+    let themeButtonElement = document.querySelector(".js-buttonChangeTheme");
+    let themeTextElement = document.querySelector(".js-themeText");
+    let body = document.querySelector(".body")
 
-button.addEventListener("click", () => {
-    header.classList.toggle("header__mainHeader--newColor");
-})
+    button.addEventListener("click", () => {
+        header.classList.toggle("header__mainHeader--newColor");
+    })
 
-photoButton.addEventListener("click", () => {
-    photo.remove();
-})
+    photoButton.addEventListener("click", () => {
+        photo.remove();
+    })
 
-themeButton.addEventListener("click", () => {
-    body.classList.toggle("body--newTheme");
-    body.classList.contains("body--newTheme") ? themeTextElement.innerText = ("Wyłącz") : themeTextElement.innerText = ("Włącz");
-})
+    themeButton.addEventListener("click", () => {
+        body.classList.toggle("body--newTheme");
+        body.classList.contains("body--newTheme") ? themeTextElement.innerText = ("Wyłącz") : themeTextElement.innerText = ("Włącz");
+    })
+}
