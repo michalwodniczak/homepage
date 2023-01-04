a3c4cc4e741708b7f17cf39f1cd427ddee406407
Michał Wodniczak <michal.wodniczak@o2.pl>
2023-01-04 17:00:41 +0100

    changed name and construction

--- js/main.js
+++ js/main.js
@@ -11,26 +11,22 @@
         photo.remove();
     }
 
-    const onChangeThemeButtonText = (body) => {
-        const themeTextElement = document.querySelector(".js-themeText");
-        body.classList.contains("body--newTheme") ? themeTextElement.innerText = ("Wyłącz") : themeTextElement.innerText = ("Włącz");
-    }
-
-    const onChangeThemeBody = () => {
-        const themeButtonElement = document.querySelector(".js-buttonChangeTheme");
+    const toggleBackground = () => {
         const body = document.querySelector(".body")
-        themeButtonElement.addEventListener("click", () => {
-            body.classList.toggle("body--newTheme");
-            onChangeThemeButtonText(body)
-        })
+        const themeTextElement = document.querySelector(".js-themeText")
+
+        body.classList.toggle("body--newTheme")
+        body.classList.contains("body--newTheme") ? themeTextElement.innerText = ("Wyłącz") : themeTextElement.innerText = ("Włącz");
     }
 
     const init = () => {
         const headerButton = document.querySelector(".js-buttonNewBackground");
         const photoButton = document.querySelector(".js-buttonPhotoRemove");
+        const themeButtonElement = document.querySelector(".js-buttonChangeTheme");
 
         headerButton.addEventListener("click", toggleHeaderColor);
         photoButton.addEventListener("click", onDeletePhoto)
+        themeButtonElement.addEventListener("click", toggleBackground)
 
     }
 
