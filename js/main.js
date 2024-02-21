5d118f3d01dd08f7f825f37940cba7b30ff90691
Michał Wodniczak <michal.wodniczak@o2.pl>
2024-02-21 16:32:43 +0100

    code cleared

--- js/main.js
+++ js/main.js
@@ -3,21 +3,21 @@
         const headerElement = document.querySelector(".js-mainHeader");
 
         headerElement.classList.toggle("header__mainHeader--secondColor")
-    }
+    };
 
     const onDeletePhoto = () => {
         const photo = document.querySelector(".js-photo");
 
         photo.remove();
-    }
+    };
 
     const toggleBackground = () => {
-        const body = document.querySelector(".body")
-        const themeTextElement = document.querySelector(".js-themeText")
+        const body = document.querySelector(".body");
+        const themeTextElement = document.querySelector(".js-themeText");
 
-        body.classList.toggle("body--darkTheme")
+        body.classList.toggle("body--darkTheme");
         themeTextElement.innerText = body.classList.contains("body--darkTheme") ? "Wyłącz" : "Włącz";
-    }
+    };
 
     const init = () => {
         const headerButton = document.querySelector(".js-buttonNewBackground");
@@ -25,10 +25,8 @@
         const themeButtonElement = document.querySelector(".js-buttonChangeTheme");
 
         headerButton.addEventListener("click", toggleHeaderColor);
-        photoButton.addEventListener("click", onDeletePhoto)
+        photoButton.addEventListener("click", onDeletePhoto);
         themeButtonElement.addEventListener("click", toggleBackground);
-
-    }
-
+    };
     init();
 }
